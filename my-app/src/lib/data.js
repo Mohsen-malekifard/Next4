49c6116e1f3c29c044941e45a6da2b62809a2f58
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-04 11:45:22 +0000

    C

---
+++ my-app/src/lib/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id : "1",
+        title : "One",
+        context : "This Description for 1"
+    },
+    {
+        id : "2",
+        title : "Two",
+        context : "This Description for 2"
+    },
+    {
+        id : "3",
+        title : "Three",
+        context : "This Description for 3"
+    },
+    {
+        id : "4",
+        title : "Four",
+        context : "This Description  for 4"
+    }
+]
+
+export default data;

608f92407840fbbbb3d098b478e1998949c723f9
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-04 11:48:11 +0000

    D

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,7 +1,13 @@
 import Image from "next/image";
+import data from "@/lib/data";
+import ProductCard from "@/components/ProductCard";
 
 export default function Home() {
   return (
-    
+    <div>
+      {data.map((p) => {
+        <ProductCard key = {p.id} product = {p} />
+      })}
+    </div>
   );
 }
